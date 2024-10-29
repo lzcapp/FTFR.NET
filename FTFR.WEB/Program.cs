e6f9041891adfdae296d52427c1511cbcc686d19
Laurence Luo <admin@lzc.app>
2024-10-29 17:55:34 +0800

    Initial Commit

---
+++ FTFR.WEB/Program.cs
@@ -0,0 +1,12 @@
+namespace FTFR.WEB {
+    public class Program {
+        public static void Main(string[] args) {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
