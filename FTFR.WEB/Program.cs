a2ab8a40fecb5182bc0ba820bd91e1429fbc03bc
Laurence Luo <admin@lzc.app>
2024-10-29 18:12:56 +0800

    Update

--- FTFR.WEB/Program.cs
+++ FTFR.WEB/Program.cs
@@ -2,9 +2,23 @@ namespace FTFR.WEB {
     public class Program {
         public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment()) {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
 
             app.Run();
         }
