3d3556fbe1c3ad22236ddc6eca172d1d82982da2
Laurence Luo <admin@lzc.app>
2024-10-31 09:57:07 +0800

    Update Program.cs

--- FTFR.Cmd/Program.cs
+++ FTFR.Cmd/Program.cs
@@ -3,26 +3,25 @@ using Microsoft.Extensions.Hosting;
 using MiHome.Net.Dto;
 using MiHome.Net.Middleware;
 using MiHome.Net.Service;
-using System.Configuration;
 using ConfigurationManager = System.Configuration.ConfigurationManager;
 
 namespace FTFR.Cmd {
     internal class Program {
         static void Main(string[] args) {
-            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
             var username = ConfigurationManager.AppSettings["Username"];
             var password = ConfigurationManager.AppSettings["Password"];
             if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password)) {
+                Console.WriteLine("请在App.config中设置用户名/密码");
                 return;
             }
 
-            var hostBuilder = Host.CreateDefaultBuilder();
+            IHostBuilder hostBuilder = Host.CreateDefaultBuilder();
             hostBuilder.ConfigureServices(it => it.AddMiHomeDriver(x =>
             {
                 x.UserName = username;
                 x.Password = password;
             }));
-            var host = hostBuilder.Build();
+            IHost host = hostBuilder.Build();
 
             var miHomeDriver = host.Services.GetService<IMiHomeDriver>();
             if (miHomeDriver == null) {
@@ -34,9 +33,9 @@ namespace FTFR.Cmd {
 
             var temp = string.Empty;
 
-            var temperatureDeviceInfo = deviceList.FirstOrDefault(it => string.Equals(it.Did, "blt.3.1jbhp2t80kg00", StringComparison.OrdinalIgnoreCase));
+            XiaoMiDeviceInfo? temperatureDeviceInfo = deviceList.FirstOrDefault(it => string.Equals(it.Did, "blt.3.1jbhp2t80kg00", StringComparison.OrdinalIgnoreCase));
             if (temperatureDeviceInfo != null) {
-                var tempSpec = miHomeDriver.Cloud.GetDeviceSpec(temperatureDeviceInfo.Model).Result;
+                MiotSpec tempSpec = miHomeDriver.Cloud.GetDeviceSpec(temperatureDeviceInfo.Model).Result;
                 var unit = tempSpec.Services[2].Properties[0].Unit;
                 var unitText = string.Empty;
                 if (unit.Equals("celsius", StringComparison.OrdinalIgnoreCase)) {
@@ -59,9 +58,9 @@ namespace FTFR.Cmd {
 
             var powerStatus = new List<bool>();
 
-            var powerDeviceInfo = deviceList.FirstOrDefault(it => string.Equals(it.Did, "768966868", StringComparison.OrdinalIgnoreCase));
+            XiaoMiDeviceInfo? powerDeviceInfo = deviceList.FirstOrDefault(it => string.Equals(it.Did, "768966868", StringComparison.OrdinalIgnoreCase));
             if (powerDeviceInfo != null) {
-                var tempSpec = miHomeDriver.Cloud.GetDeviceSpec(powerDeviceInfo.Model).Result;
+                //MiotSpec powerSpec = miHomeDriver.Cloud.GetDeviceSpec(powerDeviceInfo.Model).Result;
                 for (var i = 7; i >= 3; i--) {
                     var powerResult = miHomeDriver.Cloud.GetPropertiesAsync([
                         new GetPropertyDto() {
@@ -77,21 +76,28 @@ namespace FTFR.Cmd {
             Console.Clear();
 
             Console.WriteLine("温度：");
-            Console.WriteLine("- 水温：\t" + temp);
+            Console.WriteLine("- 水温：\t\t" + temp);
             Console.WriteLine();
             Console.WriteLine("排插：");
             for (var i = 0; i < powerStatus.Count; i++) {
                 var status = StatusBooleanToString(powerStatus[i]);
                 if (i == 0) {
-                    Console.WriteLine("- USB插座：\t" + status);
+                    Console.WriteLine("- USB插座：\t\t" + status);
                 } else {
-                    Console.WriteLine("- 插座" + i + "：\t" + status);
+                    Console.WriteLine("- 插座" + i + " (" + (Outlet)i + ") " + "：\t" + status);
                 }
             }
         }
 
         private static string StatusBooleanToString(bool status) {
             return status ? "已开启" : "已关闭";
+        }
+
+        private enum Outlet {
+            灯光 = 1,
+            水泵 = 2,
+            氧气泵 = 3,
+            加热棒 = 4
         }
     }
 }
